2b3aea4e8a8271a360245cf9905f7eb3fc6498a6
NataliaVozhdaeva <LomzNat@yandex.ru>
2023-06-24 14:28:40 +0700

    add: solving for conditions WIP

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -132,8 +132,17 @@ function isTriangle(a, b, c) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(/* rect1, rect2 */) {
-  throw new Error('Not implemented');
+function doRectanglesOverlap(rect1, rect2) {
+  if (
+    rect1.top + rect1.height < rect2.top
+    || rect2.top + rect2.height < rect1.top
+    || rect1.left + rect1.width < rect2.left
+    || rect2.left + rect2.width < rect1.left
+  ) {
+    return false;
+  }
+
+  return true;
 }
 
 /**
@@ -162,8 +171,9 @@ function doRectanglesOverlap(/* rect1, rect2 */) {
  *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
  *
  */
-function isInsideCircle(/* circle, point */) {
-  throw new Error('Not implemented');
+function isInsideCircle(circle, point) {
+  const d = Math.sqrt((point.x - circle.center.x) ** 2 + (point.y - circle.center.y) ** 2);
+  return d < circle.radius;
 }
 
 /**
@@ -177,8 +187,9 @@ function isInsideCircle(/* circle, point */) {
  *   'abracadabra'  => 'c'
  *   'entente' => null
  */
-function findFirstSingleChar(/* str */) {
-  throw new Error('Not implemented');
+function findFirstSingleChar(str) {
+  const arr = str.split('');
+  return arr.filter((elem) => arr.indexOf(elem) === arr.lastIndexOf(elem))[0];
 }
 
 /**
@@ -203,8 +214,22 @@ function findFirstSingleChar(/* str */) {
  *   5, 3, true, true   => '[3, 5]'
  *
  */
-function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
-  throw new Error('Not implemented');
+function getIntervalString(a, b, isStartIncluded, isEndIncluded) {
+  const arr = [a, b].sort();
+  if (isStartIncluded) {
+    arr.unshift('[');
+  } else {
+    arr.unshift('(');
+  }
+
+  if (isEndIncluded) {
+    arr.push(']');
+  } else {
+    arr.push(')');
+  }
+
+  arr.splice(2, 0, ', ');
+  return arr.join('');
 }
 
 /**
@@ -219,8 +244,8 @@ function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
  * 'rotator' => 'rotator'
  * 'noon' => 'noon'
  */
-function reverseString(/* str */) {
-  throw new Error('Not implemented');
+function reverseString(str) {
+  return str.split('').reverse().join('');
 }
 
 /**
